e33fd1f84a431306cabcfbf0e82bad167a40172b
thiago carneiro <carneiro.pgm@gmail.com>
2020-12-23 19:49:41 -0300

    sketch esp32

--- exerciocio_c/exerciocio_c.ino
+++ exerciocio_c/exerciocio_c.ino
@@ -1,7 +1,7 @@
+
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
+
 
 const char* ssid = "NET_2G60E226";
 const char* password = "4F60E226";
@@ -20,6 +20,10 @@ const int servomotor1 = 5;
 const int servomotor2 = 6;
 const int servomotor3 = 10;
 const int servomotor4 = 11;
+bool estadoserv1 = digitalRead(servomotor1);
+bool estadoserv2 = digitalRead(servomotor2);
+bool estadoserv3 = digitalRead(servomotor3);
+
 
 void conexao() {
 
@@ -62,110 +66,67 @@ void setup(){
 
 void loop()
 {
-
-    WiFiClient  client = server.available();
-
-    if (client) { 
-        boolean currentLineIsBlank = true;
-        while (client.connected()) {
-            if (client.available()) { 
-                char c = client.read(); 
-                HTTP_req += c;  
-                
-                if (c == '\n' && currentLineIsBlank ) { 
-
-                    if ( mainPageRequest(&HTTP_req) ) {
-                        URLValue = getURLRequest(&HTTP_req);
-                        Serial.println(HTTP_req);
-                                                 
-                        client.println("HTTP/1.1 200 OK");
-                        client.println("Content-Type: text/html");
-                        client.println("Connection: keep-alive");              //<------ ATENCAO
-                        client.println();
-                        
-                        //Conteudo da Página HTML
-                        
-                        client.println("<!DOCTYPE html>");
-                        
-                        client.println("<html>");
-                        
-                        client.println("<head>");
-                        client.println("<title>Arduino via WEB</title>");
-                        client.println("<link rel='canonical' href= 'https://getbootstrap.com/docs/4.5/examples/checkout/'>");
-                        client.println("<link href='static/css/assets/dist/css/bootstrap.min.css' rel='stylesheet>");
-                        client.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity=sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>");
-                        client.println( "<link href='https://fonts.googleapis.com/css2?family=Titillium+Web:wght@30'0&di'splay=swap' rel='stylesheet'>");
-                        client.println("</head>");
-                       
-                        client.println("<body style='background: rgb(161, 161, 159);  font-family: 'Titillium Web', sans-serif;>'");
-                        client.println("<div style='background-color: rgb(127, 161, 116); margin-left: 10%; margin-right: 10%; margin-top: 5%; padding-left: 30%; padding-top: 5%; padding-bottom: 5%;' class='conteiner'>");
-                        client.println("<h5>PAINEL DE CONTROLE<h5>");
-                        client.println("<form action='/' method='post'>");
-                        client.println("<button type='button' class='btn btn-danger' name='comando' value='1ON'>Liga</button>");
-                        client.println("<button type='button' class='btn btn-warning' name=' comando' value='1OFF'>Desliga</button>");
-                        client.println("<button type='button' class='btn btn-success'>opcional</button>");              
-                        client.println("</form>");
-                        client.println("</div>");
-                        client.println("</body>");
-                        
-                        client.println("</html>");
-
-                        
-
                     
-        } else {
-
-                  Serial.println(HTTP_req);
-                  client.println("HTTP/1.1 200 OK");
-              }
-              HTTP_req = "";    
-              break;
-          }
-          
+           
+WiFiClient client = server.available();   
+if (client) {
+          Serial.println("Novo Cliente.");
+          String currentLine = "";    
+     while (client.connected()) {
+          char c = client.read();
+          Serial.write(c);
           if (c == '\n') {
-              currentLineIsBlank = true;
-          } 
-          else if (c != '\r') {
-              currentLineIsBlank = false;
-          }
-            }
-              } 
-    delay(1);     
-    client.stop(); 
-  } 
+                if (currentLine.length() == 0) {
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-type:text/html");
+                client.println();
+                             
+                client.println("<!DOCTYPE html>");
+                
+                client.println("<html>");
+                
+                client.println("<head>");
+                client.println("<title>Arduino via WEB</title>");
+                client.println("<link rel='canonical' href= 'https://getbootstrap.com/docs/4.5/examples/checkout/'>");
+                client.println("<link href='static/css/assets/dist/css/bootstrap.min.css' rel='stylesheet>");
+                client.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity=sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>");
+                client.println( "<link href='https://fonts.googleapis.com/css2?family=Titillium+Web:wght@30'0&di'splay=swap' rel='stylesheet'>");
+                client.println("</head>");
+               
+                client.println("<body style='background: rgb(161, 161, 159);  font-family: 'Titillium Web', sans-serif;>'");
+                client.println("<div style='background-color: rgb(127, 161, 116); margin-left: 10%; margin-right: 10%; margin-top: 5%; padding-left: 30%; padding-top: 5%; padding-bottom: 5%;' class='conteiner'>");
+                client.println("<h5>PAINEL DE CONTROLE<h5>");
+                client.println("<form action='/' method='GET'>");
+                client.println("<button type='button' class='btn btn-danger' name='comando' value='1ON'>Liga</button>");
+                client.println("<button type='button' class='btn btn-warning' name=' comando' value='2ON'>Desliga</button>");
+                client.println("<button type='button' class='btn btn-success' name='comando' value='3ON'>opcional</button>");              
+                client.println("</form>");
+                client.println("</div>");
+                client.println("</body>");
+                client.println("</html>");                    
+                client.println();
+                break;
+      
+                } else{ 
+                currentLine = "";
+                }
+                } else if (c != '\r') {
+                currentLine += c;
+                }
+      
+                if (currentLine.endsWith("GET /1ON")) {
+                digitalWrite(servomotor1, HIGH);
+                }
+                
+                if (currentLine.endsWith("GET /2ON")) {
+                digitalWrite(servomotor2, HIGH);
+                }
+                
+                if (currentLine.endsWith("GET /3ON")) {
+                digitalWrite(servomotor1, HIGH);
+                }
+                client.stop();
+                Serial.println("Cliente desconectado.");
+                }
 }
-
-
-String getURLRequest(String *requisicao) {
-int inicio, fim;
-String retorno;
-
-  inicio = requisicao->indexOf("GET") + 3;
-  fim = requisicao->indexOf("HTTP/") - 1;
-  retorno = requisicao->substring(inicio, fim);
-  retorno.trim();
-
-  return retorno;
-}
-
-bool mainPageRequest(String *requisicao) {
-String valor;
-bool retorno = false;
-
-  valor = getURLRequest(requisicao);
-  valor.toLowerCase();
-
-  if (valor == "/") {
-     retorno = true;
-  }
-
-  if (valor.substring(0,2) == "/?") {
-     retorno = true;
-  }  
-
-  if (valor.substring(0,10) == "/index.htm") {
-     retorno = true;
-  }  
-
-  return retorno;
 }
